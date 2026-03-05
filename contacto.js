d2d8bd4de7328b063e8145ac2352cd870ec6f181
Conscientemente <conscientemente524@gmail.com>
2026-03-04 22:13:15 -0500

    Create contacto.js

---
+++ contacto.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const questions = document.querySelectorAll(".faq-question");
+
+  questions.forEach(question => {
+
+    question.addEventListener("click", function () {
+
+      const answer = this.nextElementSibling;
+
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      }
+
+    });
+
+  });
+
+});
