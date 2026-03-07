67796187c4f5d46feae310e89f991ece9dcb5375
Conscientemente <conscientemente524@gmail.com>
2026-03-06 22:23:30 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,18 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-menu a");
+const header = document.querySelector(".site-header");
 
 menuToggle.addEventListener("click", function(){
   navMenu.classList.toggle("active");
   menuToggle.classList.toggle("open");
+  header.classList.toggle("menu-open");
 });
-
-/* cerrar menú al tocar una opción */
 
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     navMenu.classList.remove("active");
     menuToggle.classList.remove("open");
+    header.classList.remove("menu-open");
   });
 });
